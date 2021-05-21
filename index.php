a2f4b0ee0ac36dbcacdbdf601886ff23cb58ec52
Ben Miles <bcgm3studios@gmail.com>
2021-05-21 17:30:19 -0400

    Move Routing to Logic

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require "data/variables.php";
 require "data/credentials.php";
 require "logic/database.php";
+require "logic/routing.php";
 
 // TODO: Lightbox for individual posts
 // TODO: Lazy loading (and SQL)?
@@ -11,22 +12,3 @@ require "logic/database.php";
 // TODO: User login (see salt and hash)
 // TODO: Add new post / File upload -- Start out simple, no image editing in-app, use Camera app for that. Then add ImageMagick(?) for automating thumbnails
 // TODO: Interactivity? Think I want to avoid "likes" and maybe even "comments"
-
-// Routing
-$request = $_SERVER['REQUEST_URI'];
-switch ($request) {
-    case '' :
-    case '/' :
-        require "view/portfolio.php";
-        break;
-	case '/login' :
-		require "view/login.php";
-		break;
-	case '/post' :
-		require "view/post.php";
-		break;
-    default:
-        http_response_code(404);
-        require "view/404.php";
-        break;
-}
---
+++ logic/routing.php
@@ -0,0 +1,25 @@
+<?php
+// Routing
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case '' :
+    case '/' :
+        require "view/portfolio.php";
+        break;
+	case '/login' :
+		require "view/login.php";
+		break;
+	case '/signup' :
+		require "view/signup.php";
+		break;
+	case '/signup-logic' :
+		require "logic/signup.php";
+		break;
+	case '/post' :
+		require "view/post.php";
+		break;
+    default:
+        http_response_code(404);
+        require "view/404.php";
+        break;
+}
