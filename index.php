31689adcc24d5875423610a92c03aed149e24844
Ben Miles <bcgm3studios@gmail.com>
2021-05-24 12:31:58 -0400

    Reformat TODOs

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ require "data/credentials.php";
 require "logic/database.php";
 require "logic/routing.php";
 
-// TODO: Lightbox for individual posts
-// TODO: Lazy loading (and SQL)?
-// TODO: Profile
-// TODO: Update URL on click of images
-// TODO: User login (see salt and hash)
-// TODO: Add new post / File upload -- Start out simple, no image editing in-app, use Camera app for that. Then add ImageMagick(?) for automating thumbnails
-// TODO: Interactivity? Think I want to avoid "likes" and maybe even "comments"
+// TODOs:
+// - Lightbox for individual posts
+// - Lazy loading (and SQL)?
+// - Profile
+// - Update URL on click of images
+// - User login (see salt and hash)
+// - Add new post / File upload -- Start out simple, no image editing in-app, use Camera app for that. Then add ImageMagick(?) for automating thumbnails
+// - Interactivity? Think I want to avoid "likes" and maybe even "comments"
+// - Add fields to Signup View
